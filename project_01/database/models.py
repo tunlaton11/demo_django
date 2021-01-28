3ab3825b6b9008a6de5112cbc956cc8ed2ce2690
Synthesizer <tunlaton11@gmail.com>
2021-01-28 17:51:51 +0700

    commit

---
+++ project_01/database/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Rating(models.Model):
+    email = models.CharField(max_length=50)
+    tel = models.CharField(max_length=15)
+    comment = models.CharField(max_length=200)
+    rating = models.IntegerField()
