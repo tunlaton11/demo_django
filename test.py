151b4ddc3b13ebe488e656c770a75a3e4ef2a41c
Synthesizer <tunlaton11@gmail.com>
2021-05-06 21:51:54 +0700

    commit

--- test.py
+++ test.py
@@ -10,13 +10,13 @@ def createBooks(shelf):
     for i in range(1000):
         page = str(random.randrange(99,888))
 
-    titleToken = random.sample(allWords,random.randint(3,5)) 
-    postfix = random.randint(0,len(titleToken)-1)
-    if random.choice([1,2,3,4,5]) == 3:
-        titleToken[postfix]  =titleToken[postfix] + '\'s'  
-    title = ' '.join(titleToken)
-    title = title.capitalize()
-    shelf.write(':'.join([title,page])+'\n')
+        titleToken = random.sample(allWords,random.randint(3,5)) 
+        postfix = random.randint(0,len(titleToken)-1)
+        if random.choice([1,2,3,4,5]) == 3:
+            titleToken[postfix]  =titleToken[postfix] + '\'s'  
+        title = ' '.join(titleToken)
+        title = title.capitalize()
+        shelf.write(':'.join([title,page])+'\n')
 
 shelf = open('book_shelf.txt','w')
 createBooks(shelf)
@@ -24,16 +24,25 @@ shelf.close()
 
 # เปิด file book_shelf.txt และ หาจำนวนหน้าเฉลี่ยต่อเล่ม ของหนังสือทุกเล่มซึ่งมี postfix 's อยู่ในชื่อ
 ## Your code begins here ##
-def book_postfix(book_read):    
-    book_name = book_read[0:book_read.find(":")].split(' ')
-    for i in book_name:
-        if i[-2:] == '\'s':
-            return 'Book: '+ i + '\nHas: ' + book_read[book_read.index(':') + 1:]
-        else:
-            continue
-    return 'not found postfix \'s book'
+
+def book_postfix(book_read):
+    postfix_book = 0
+    pages = 0
+    for i in book_read:
+        book_name = i[0:i.find(":")].split(' ')
+        for j in book_name:
+            if j[-2:] == '\'s':
+                postfix_book += 1
+                pages += int(i[i.find(':') + 1:])
+            else:
+                continue
+    avg_pages = pages / postfix_book
+    return avg_pages
 
 book = open('book_shelf.txt','r')
-book_read = book.read()
+book_read = book.read().split('\n')
 print(book_postfix(book_read))
 book.close()
+
+
+
